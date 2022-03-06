97f596896d0d488439ce7e3ef488b817816868de
paradajz <2544094+paradajz@users.noreply.github.com>
2022-03-06 10:52:02 +0100

    update copyright year

--- src/SysExConf.cpp
+++ src/SysExConf.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2017-2020 Igor Petrovic
+    Copyright 2017-2022 Igor Petrovic
 
     Permission is hereby granted, free of charge, to any person obtaining
     a copy of this software and associated documentation files (the "Software"),
