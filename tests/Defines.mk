98d1ec0343c560a0ec65eabec05ddfaea1d1216a
paradajz <2544094+paradajz@users.noreply.github.com>
2023-06-10 22:48:36 +0200

    rewrite the build system in cmake, update ci config

--- tests/Defines.mk
+++
@@ -1,2 +0,0 @@
-#common defines
-DEFINES += TEST
--- tests/src/test/test.cpp
+++ tests/src/test/test.cpp
@@ -1,10 +1,34 @@
-#include "framework/Framework.h"
-#include "SysExConf.h"
-#include "src/SysExTesting.h"
+#include "tests/Common.h"
+#include "SysExConf/SysExConf.h"
 
-#define SYS_EX_CONF_M_ID_0 0x00
-#define SYS_EX_CONF_M_ID_1 0x53
-#define SYS_EX_CONF_M_ID_2 0x43
+#define SYS_EX_CONF_M_ID_0              0x00
+#define SYS_EX_CONF_M_ID_1              0x53
+#define SYS_EX_CONF_M_ID_2              0x43
+#define SECTION_0_PARAMETERS            10
+#define SECTION_1_PARAMETERS            6
+#define SECTION_2_PARAMETERS            33
+#define SECTION_0_MIN                   0
+#define SECTION_0_MAX                   50
+#define SECTION_1_MIN                   0
+#define SECTION_1_MAX                   0
+#define SECTION_2_MIN                   0
+#define SECTION_2_MAX                   0
+#define TEST_BLOCK_ID                   0
+#define TEST_SECTION_SINGLE_PART_ID     0
+#define TEST_SECTION_MULTIPLE_PARTS_ID  2
+#define TEST_SECTION_NOMINMAX           1
+#define TEST_INDEX_ID                   5
+#define TEST_MSG_PART_VALID             0
+#define TEST_MSG_PART_INVALID           10
+#define TEST_INVALID_PARAMETER_B0S0     15
+#define TEST_NEW_VALUE_VALID            25
+#define TEST_NEW_VALUE_INVALID          100
+#define TEST_VALUE_GET                  3
+#define CUSTOM_REQUEST_ID_VALID         54
+#define CUSTOM_REQUEST_ID_INVALID       55
+#define CUSTOM_REQUEST_ID_ERROR_READ    56
+#define CUSTOM_REQUEST_ID_NO_CONN_CHECK 57
+#define CUSTOM_REQUEST_VALUE            1
 
 #define SYSEX_PARAM(value)                                                                                                         \
     (((value & 0xFF) >> 7) & 0x01) ? ((((value >> 8) & 0xFF) << 1) & 0x7F) | 0x01 : ((((value >> 8) & 0xFF) << 1) & 0x7F) & ~0x01, \
